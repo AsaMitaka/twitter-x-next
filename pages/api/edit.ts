18bccb0bf5e45f6c29e5873ee7a16ac5b67e25c1
AsaMitaka <cepelits@gmail.com>
2023-10-16 19:46:13 +0300

    api edit

---
+++ pages/api/edit.ts
@@ -0,0 +1,30 @@
+import serverAuth from '@/libs/serverAuth';
+import { NextApiRequest, NextApiResponse } from 'next';
+import prismadb from '@/libs/prismadb';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'PATCH') {
+    return res.status(405).end();
+  }
+
+  try {
+    const { currentUser } = await serverAuth(req);
+    const { bio, profileImage, coverImage } = req.body;
+
+    const updatedUser = await prismadb.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        bio,
+        coverImage,
+        profileImage,
+      },
+    });
+
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    console.warn(error);
+    return res.status(400).end();
+  }
+}
