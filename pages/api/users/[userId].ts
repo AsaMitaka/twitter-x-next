8f3e401544d27528f7ac55a57866e4bb869212c1
AsaMitaka <cepelits@gmail.com>
2023-10-16 17:12:49 +0300

    api users, userId

---
+++ pages/api/users/[userId].ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prismadb from '@/libs/prismadb';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    return res.status(405).end();
+  }
+
+  try {
+    const { userId } = req.query;
+    if (!userId || typeof userId !== 'string') {
+      throw new Error('Invalid user ID');
+    }
+
+    const user = await prismadb.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    const followingCount = await prismadb.user.count({
+      where: {
+        followingIds: {
+          has: userId,
+        },
+      },
+    });
+
+    return res.status(200).json({ ...user, followingCount });
+  } catch (error) {
+    console.warn(error);
+    return res.status(400).end();
+  }
+}
