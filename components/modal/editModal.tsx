2af3cb7913b43a3499fab12130d1f78fb1afa2e8
AsaMitaka <cepelits@gmail.com>
2023-10-16 19:45:43 +0300

    modal

---
+++ components/modal/editModal.tsx
@@ -0,0 +1,89 @@
+import { useCallback, useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import axios from 'axios';
+
+import Modal from './modal';
+import UploadImage from '../uploadImage';
+import Input from '../ui/input';
+
+import useEditModal from '@/hooks/useEditModal';
+import useCurrentUser from '@/hooks/useCurrent';
+import useUser from '@/hooks/useUser';
+
+const EditModal = () => {
+  const { data: currentUser } = useCurrentUser();
+  const { mutate: mutateFetchUser } = useUser(currentUser?.id);
+  const editModal = useEditModal();
+
+  const [profileImage, setProfileImage] = useState('');
+  const [coverImage, setCoverImage] = useState('');
+  const [bio, setBio] = useState('');
+
+  useEffect(() => {
+    setProfileImage(currentUser?.profileImage);
+    setCoverImage(currentUser?.coverImage);
+    setBio(currentUser?.bio);
+  }, [currentUser?.bio, currentUser?.profileImage, currentUser?.coverImage]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.patch('/api/edit', {
+        bio,
+        coverImage,
+        profileImage,
+      });
+      mutateFetchUser();
+
+      toast.success('Updated');
+
+      editModal.onClose();
+    } catch (error) {
+      console.warn(error);
+      toast.error('Something went wrong');
+    } finally {
+      setIsLoading(false);
+    }
+  }, [bio, coverImage, editModal, mutateFetchUser, profileImage]);
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Input
+        disabled={isLoading}
+        onChange={(e) => setBio(e.target.value)}
+        placeholder="Bio"
+        type="text"
+        value={bio}
+      />
+      <UploadImage
+        disabled={isLoading}
+        label="Upload profile image"
+        onChange={(image) => setProfileImage(image)}
+        value={profileImage}
+      />
+      <UploadImage
+        disabled={isLoading}
+        label="Upload cover image"
+        onChange={(image) => setCoverImage(image)}
+        value={coverImage}
+      />
+    </div>
+  );
+
+  return (
+    <Modal
+      actionLabel="Save"
+      body={bodyContent}
+      disabled={isLoading}
+      isOpen={editModal.isOpen}
+      onClose={editModal.onClose}
+      onSubmit={onSubmit}
+      title="Edit Profile"
+    />
+  );
+};
+
+export default EditModal;
---
+++ components/modal/loginModal.tsx
@@ -0,0 +1,85 @@
+import { useCallback, useState } from 'react';
+import Modal from './modal';
+import Input from '../ui/input';
+import useSignupModal from '@/hooks/useSignupModal';
+import useLoginModal from '@/hooks/useLoginModal';
+import { signIn } from 'next-auth/react';
+import toast from 'react-hot-toast';
+
+const LoginModal = () => {
+  const useLogin = useLoginModal();
+  const useSignup = useSignupModal();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
+  const handleTarget = useCallback(() => {
+    if (isLoading) return;
+
+    useLogin.onClose();
+    useSignup.onOpen();
+  }, [useLogin, useSignup, isLoading]);
+
+  const onSubmit = useCallback(() => {
+    try {
+      setLoading(true);
+
+      toast.success('Logged in');
+      signIn('credentials', {
+        email,
+        password,
+      });
+      useLogin.onClose();
+    } catch (error) {
+      console.warn(error);
+      toast.error('Something went wrong');
+    } finally {
+      setLoading(false);
+    }
+  }, [useLogin, email, password]);
+
+  const bodyContent = (
+    <div className="flex flex-col gap-2">
+      <Input
+        type="email"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        disabled={isLoading}
+      />
+      <Input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        disabled={isLoading}
+      />
+    </div>
+  );
+
+  const footerContent = (
+    <div className="mt-1">
+      <p className="text-white text-md">
+        You dont have an account?
+        <span className="ml-3 cursor-pointer hover:underline" onClick={handleTarget}>
+          Sign Up
+        </span>
+      </p>
+    </div>
+  );
+
+  return (
+    <Modal
+      actionLabel="Login"
+      body={bodyContent}
+      disabled={isLoading}
+      footer={footerContent}
+      isOpen={useLogin.isOpen}
+      onClose={useLogin.onClose}
+      onSubmit={onSubmit}
+      title="Login"
+    />
+  );
+};
+
+export default LoginModal;
