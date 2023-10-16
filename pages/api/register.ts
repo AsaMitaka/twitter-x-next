627edd67376c1b097af1ee25a438e10c356f476d
AsaMitaka <cepelits@gmail.com>
2023-10-16 17:12:25 +0300

    api next-auth

---
+++ pages/api/register.ts
@@ -0,0 +1,28 @@
+import bcrypt from 'bcrypt';
+import { NextApiRequest, NextApiResponse } from 'next';
+import prismadb from '@/libs/prismadb';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    return res.status(405).end();
+  }
+
+  try {
+    const { email, username, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prismadb.user.create({
+      data: {
+        email,
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.warn(error);
+    return res.status(400).end();
+  }
+}
